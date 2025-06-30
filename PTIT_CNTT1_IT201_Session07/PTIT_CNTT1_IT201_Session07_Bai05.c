07ad2cd099039225606fa21446c01fefb5a38781
Huy <maihuylc@gmail.com>
2025-06-30 20:26:30 +0700

    session07

--- PTIT_CNTT1_IT201_Session07/PTIT_CNTT1_IT201_Session07_Bai05.c
+++ PTIT_CNTT1_IT201_Session07/PTIT_CNTT1_IT201_Session07_Bai05.c
@@ -20,9 +20,9 @@ int partition(int arr[] , int l , int r){
 }
 void quickSort(int arr[], int l, int r) {
     if (l < r) {
-        int pi = partition(arr, l, r);
-        quickSort(arr, l, pi - 1);
-        quickSort(arr, pi, r);
+        int i = partition(arr, l, r);
+        quickSort(arr, l, i - 1);
+        quickSort(arr, i, r);
     }
 }
 int main(){
